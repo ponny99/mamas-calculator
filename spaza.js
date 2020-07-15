7b7f24fe6fad79588f09adb6f265cd3c3e7d166e
Selfmade20e <tebogo1686@gmail.com>
2020-07-15 12:01:43 +0200

    creting a culculator

---
+++ spaza.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#one').click(function(){ 
+        document.forms.display.value += 1; 
+  });
+    $('#two').click(function(){ 
+        document.forms.display.value += 2; 
+    });
+     $('#three').click(function(){ 
+        document.forms.display.value += 3; 
+    });
+    $('#four').click(function(){ 
+        document.forms.display.value += 4; 
+    });       
+})
